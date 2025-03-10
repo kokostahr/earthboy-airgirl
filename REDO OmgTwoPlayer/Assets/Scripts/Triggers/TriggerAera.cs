a682eeeb381ace572ac40f844672d4b28690a1e0
kokostahr <starking.nn@gmail.com>
2025-03-10 20:06:06 +0200

    added a few testers ti the unity scene

--- REDO OmgTwoPlayer/Assets/Scripts/Triggers/TriggerAera.cs
+++ REDO OmgTwoPlayer/Assets/Scripts/Triggers/TriggerAera.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerAera : MonoBehaviour
@@ -12,20 +10,50 @@ public class TriggerAera : MonoBehaviour
     //Going to assign the object that will activate, whether its a platform or door...will see
     public GameObject objectToActivate;
 
-
-
-
-
-
-
-    void Start()
+    //LET'S START USING TRIGGERS! 
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player1")) //why is it highlighted red bruh
+        {
+            //if player 1 enters, then set the bool to true.
+            playe1Inside = true;    
+        }
+
+        //do the same for player two
+        if (other.CompareTag("Player2"))
+        {
+            playe2Inside = true;
+        }
+        ActivateObject();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player1")) //why is it highlighted red bruh
+        {
+            //if player 1 enters, then set the bool to true.
+            playe1Inside = false;
+        }
+
+        //do the same for player two
+        if (other.CompareTag("Player2"))
+        {
+            playe2Inside = false;
+        }
+        ActivateObject();
+    }
+
+    void ActivateObject()
+    {
+        //method that will determine if both players are inside the trigger, and react accordingly
+        if (playe1Inside && playe2Inside) //if both are inside, then
+        {
+            //activate the platform or the door. Need better code? will i move these things in code or...with the animator. we shall see lol
+            objectToActivate.SetActive(true);
+        }
+        else //IF BOTH aren't inside, then the object will NOOT appear for now
+        {
+            objectToActivate.SetActive(false);
+        }
     }
 }
