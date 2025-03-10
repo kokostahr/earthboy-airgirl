527325f3c360f0de63f9c86f0e342190a9272ac5
kokostahr <starking.nn@gmail.com>
2025-03-10 19:52:02 +0200

    adding to the triggeraera script

--- REDO OmgTwoPlayer/Assets/Scripts/Triggers/TriggerAera.cs
+++ REDO OmgTwoPlayer/Assets/Scripts/Triggers/TriggerAera.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class TriggerAera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Need code that will detect when both players are in the relevant trigger areas
+    //Using bools to perform checks
+    private bool playe1Inside = false;
+    private bool playe2Inside = false;
+
+    //Going to assign the object that will activate, whether its a platform or door...will see
+    public GameObject objectToActivate;
+
+
+
+
+
+
+
     void Start()
     {
         
